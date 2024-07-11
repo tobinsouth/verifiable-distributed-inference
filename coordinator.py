8521dbc692446463f5682c258bbf3e985cb3343c
chenneking <39014772+chenneking@users.noreply.github.com>
2024-07-11 01:11:18 -0400

    Created worker & coordinator

---
+++ coordinator.py
@@ -0,0 +1,198 @@
+import json
+import sys
+import socket
+import threading
+import time
+from typing import Tuple
+
+import numpy as np
+import torch
+
+from modules.connection_handler import CoordinatorConnectionHandler
+from modules.witness_manager import WitnessManager
+from modules.model_processing import Processor
+from modules.model_training import Trainer
+from modules.model_proving import Prover
+from modules.file_manager import FileManager
+from torch import nn
+from utils.helpers import conditional_print, decode_b64_to_np_array, encode_np_array_to_b64
+
+VERBOSE = True
+MODEL_ID = "model_0"
+MODEL_STORAGE_DIR = "./shared-storage/shards"
+RUN_LOGIC = True
+
+
+class Coordinator:
+    def __init__(self, address: Tuple[str, int], num_shards: int):
+        self.connections: dict[str, threading.Thread] = {}
+        self.handlers: dict[str, CoordinatorConnectionHandler] = {}
+        self.connection_to_inbound_address: dict = {}
+        self.address: Tuple[str, int] = address
+        self.socket: socket = None
+        self.witness_manager: WitnessManager = WitnessManager()
+        self.num_shards: int = num_shards
+        self.num_ready_nodes: int = 0
+
+    def run(self) -> None:
+        self.open_socket()
+        trainer = Trainer()
+        # Optionally train the model here.
+        # trainer.train()
+        # trainer.eval()
+
+        model: nn.Module = trainer.model
+
+        # Generate a dummy input tensor. Needed for the processing of the model.
+        dummy_input: torch.Tensor = trainer.get_dummy_input()
+
+        conditional_print("[PREPROCESSING] Loaded model", VERBOSE)
+
+        # Process the model and turn it into shards.
+        model_processor = Processor(
+            model=model,
+            sample_input=dummy_input
+        )
+        model_processor.shard(self.num_shards)
+
+        conditional_print("[PREPROCESSING] Sharded model", VERBOSE)
+
+        model_processor.save(
+            model_id=MODEL_ID,
+            storage_dir=MODEL_STORAGE_DIR
+        )
+
+        conditional_print("[PREPROCESSING] Saved model (shards)", VERBOSE)
+
+        connection_counter: int = 0
+
+        conditional_print("[LOGIC] Starting connection loop", VERBOSE)
+        try:
+
+            while connection_counter < self.num_shards:
+                socket_connection, socket_address = self.socket.accept()
+                conditional_print(f"[LOGIC] Accepted connection from: {socket_address}", VERBOSE)
+                if connection_counter > self.num_shards - 1:
+                    print(f"[ISSUE] There are more connections than available shards: {self.num_shards} shards are taken."
+                          f" Shutting down connection.")
+                    socket_connection.close()
+                    continue
+                # Not needed anymore, as we make file retrieval based on `shard_id` and `model_id`.
+                # shard_path: str = model_processor.shard_paths[connection_counter]
+                conn_handler = CoordinatorConnectionHandler(
+                    connection=socket_connection,
+                    address=socket_address,
+                    initiating_node=self,
+                    shard_id=connection_counter,
+                    model_id=MODEL_ID
+                )
+                conn_thread = threading.Thread(
+                    target=conn_handler.run
+                )
+                conn_key = f"{socket_address[0]}:{socket_address[1]}"
+                # Store the connection thread
+                self.connections[conn_key] = conn_thread
+                # Store connection handler. Required to later send messages to each node using its .send() method.
+                self.handlers[conn_key] = conn_handler
+                conn_thread.start()
+                connection_counter += 1
+
+            # Loops until all nodes have sent a ready message
+            while self.num_ready_nodes != self.num_shards:
+                conditional_print(f'[LOGIC] Not all nodes ready to process inference request(s). '
+                                  f'Retrying in 5s.', VERBOSE)
+                time.sleep(5)
+
+            print('[LOGIC] Setup completed. Inference runs can now be served.')
+            first_node_handler: CoordinatorConnectionHandler = list(self.handlers.values())[0]
+            second_node_handler: CoordinatorConnectionHandler = list(self.handlers.values())[1]
+            np_arr = Trainer.get_dummy_input().cpu().numpy()
+            encoded_np_arr = encode_np_array_to_b64(np_arr)
+            message: bytes = b'run_inference|' + encoded_np_arr
+            first_node_handler.send_bytes(message)
+
+            time.sleep(10)
+
+            first_node_handler.send(f'get_proof|1ca6dd0091304ca9b985b615a4998eaa_0')
+            second_node_handler.send(f'get_proof|3f1f607fdd6f485588c9ea7533c2c136_0')
+
+        except KeyboardInterrupt:
+            self.socket.close()
+            for thread in self.connections.values():
+                thread.join()
+            sys.exit(0)
+        finally:
+            self.socket.close()
+            for thread in self.connections.values():
+                thread.join()
+
+    def open_socket(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        self.socket.bind(address)
+        self.socket.listen()
+
+    # Intermediate function that adds a witness_id to the witness_manager.
+    def store_witness(self, witness_id: str, shard_id: int) -> None:
+        self.witness_manager.add_witness(witness_id, shard_id)
+
+    # Requests a proof for a specific witness_id.
+    def request_single_proof(self, witness_id: str) -> None:
+        shard_id = self.witness_manager.get_shard_by_witness_id(witness_id)
+        for handler in self.handlers.values():
+            if handler.shard_id == shard_id:
+                handler.send(f'get_proof|{witness_id}')
+                pass
+
+    # Requests proofs from all nodes for one specific run_id.
+    # To be used when we can't pinpoint which node is running the model incorrectly.
+    def request_inference_run_proof(self, run_id: int) -> None:
+        # Pull in all witness_ids for a specific run
+        witness_id_list: list[str] = self.witness_manager.get_witnesses_by_run_id(run_id)
+
+        # Request proof from all nodes
+        for witness_id in witness_id_list:
+            witness_shard_id = self.witness_manager.get_shard_by_witness_id(witness_id)
+            for handler in self.handlers.values():
+                if handler.shard_id == witness_shard_id:
+                    handler.send(f'get_proof|{witness_id}')
+
+    # Intermediate function that verifies the proof
+    def verify_proof(self, proof_path: str, shard_id: int, model_id: str) -> None:
+        # Only the proof_path is passed, and the other two paths are derived, as they stay static
+        # for all future proofs.
+        Prover.verify_proof(
+            proof_path=proof_path,
+            settings_path=FileManager.get_settings_path_static(shard_id, model_id),
+            vk_path=FileManager.get_vk_path_static(shard_id, model_id)
+        )
+
+    # Saves the final inference output to a file
+    def save_final_inference_output(self, raw_output_data: bytes, run_id: int) -> None:
+        file_path: str = FileManager.get_final_output_path(MODEL_ID, run_id)
+        output_array: np.ndarray = decode_b64_to_np_array(raw_output_data)
+        raw_output_file_data: dict = dict(
+            output_shapes=[output_array.shape],
+            output_data=[o.reshape([-1]).tolist() for o in output_array]
+        )
+        with open(file_path, 'w') as f:
+            json.dump(raw_output_file_data, f)
+            f.close()
+        conditional_print(f'Wrote final inference output to: {file_path}', VERBOSE)
+
+    # Adds 1 to the number of ready nodes.
+    def register_ready_node(self) -> None:
+        self.num_ready_nodes += 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f'Usage: coordinator.py <host> <port> <num_shards>')
+        sys.exit(0)
+
+    address = (sys.argv[1], int(sys.argv[2]))
+    num_shards = int(sys.argv[3])
+    coordinator = Coordinator(
+        address=address,
+        num_shards=num_shards
+    )
+    coordinator.run()
---
+++ worker.py
@@ -0,0 +1,274 @@
+import asyncio
+import sys
+import socket
+import threading
+import time
+import uuid
+import json
+
+import numpy as np
+import onnxruntime as ort
+from typing import Tuple
+from modules.connection_handler import CoordinatorConnectionHandler, WorkerConnectionHandler
+from modules.model_proving import Prover
+from modules.file_manager import FileManager
+from utils.helpers import conditional_print, decode_b64_to_np_array, encode_np_array_to_b64
+
+
+VERBOSE: bool = True
+# EZKL configuration
+# We want the input and outputs to be "publicly" visible in the
+INPUT_VISIBILITY: str = "public"
+OUTPUT_VISIBILITY: str = "public"
+PARAM_VISIBILITY: str = "hashed"
+OPTIMIZATION_GOAL: str = "resources"
+INPUT_SCALE = None
+PARAM_SCALE = None
+
+
+class Worker:
+    def __init__(self, address: Tuple[str, int], coordinator_address: Tuple[str, int], node_role: str):
+
+        self.shard_id: int = None
+        self.model_id: str = None
+
+        # Address other nodes use to connect to worker
+        self.address: Tuple[str, int] = address
+
+        # "Outbound" connection to coordinator
+        self.coordinator_address: Tuple[str, int] = coordinator_address
+        self.coordinator_socket: socket = None
+        self.coordinator_conn_handler = None
+        self.conn_coordinator_thread = None
+
+        # "Outbound" connection to subsequent worker
+        self.outbound_worker_address: Tuple[str, int] = None
+        self.outbound_worker_socket: socket = None
+        self.outbound_worker_conn_handler = None
+        self.outbound_conn_worker_thread = None
+
+        # "Inbound" connection to previous worker
+        self.inbound_worker_address: Tuple[str, int] = None
+        self.inbound_worker_socket: socket = None
+        self.inbound_worker_conn_handler = None
+        self.inbound_conn_worker_thread = None
+
+        # TODO: think of a smarter way of assigning nodes!
+        self.node_role: str = node_role
+
+        self.model_runtime_session: ort.InferenceSession = None
+
+        self.file_manager: FileManager = None
+
+        self.prover: Prover = None
+
+        # Counts the number of inference runs. Counter gets appended to uuid generated during inference run.
+        self.inference_run_counter: int = 0
+
+    def run(self):
+        try:
+            # Open socket to accept connection from previous worker
+            if not self.node_role == "FIRST":
+                self.open_worker_socket()
+
+            self.connect_to_coordinator()
+            self.coordinator_conn_handler = CoordinatorConnectionHandler(
+                connection=self.coordinator_socket,
+                address=coordinator_address,
+                initiating_node=self
+            )
+
+            # Spawn first thread to handle connection from worker to coordinator
+            self.conn_coordinator_thread = threading.Thread(
+                target=self.coordinator_conn_handler.run
+            )
+            self.conn_coordinator_thread.start()
+
+            # Report the address that a worker can accept connections from. Important for the previous worker
+            if not self.node_role == "FIRST":
+                self.coordinator_conn_handler.send(f"set_inbound_connection_address|{self.address[0]}|{self.address[1]}")
+
+            # The last node will not need to obtain and/or connect to another node.
+            # All other worker nodes should obtain the address of their subsequent node to connect to.
+            if not self.node_role == "LAST":
+                self.coordinator_conn_handler.send("get_subsequent_worker_address")
+
+            if not self.node_role == "FIRST":
+                self.inbound_worker_socket, self.inbound_worker_address = self.inbound_worker_socket.accept()
+                self.inbound_worker_conn_handler = WorkerConnectionHandler(
+                    connection=self.inbound_worker_socket,
+                    address=self.inbound_worker_address,
+                    initiating_node=self
+                )
+                self.inbound_conn_worker_thread = threading.Thread(
+                    target=self.inbound_worker_conn_handler.run
+                )
+                self.inbound_conn_worker_thread.start()
+                conditional_print(f"[LOGIC] Accepted connection from {self.inbound_worker_address}", VERBOSE)
+
+            # The last node in the inference chain doesn't have a subsequent node to connect to.
+            if not self.node_role == "LAST":
+                # Spawn second thread to handle connection from worker to next worker (in the inference chain)
+                self.connect_to_worker()
+                self.outbound_worker_conn_handler = WorkerConnectionHandler(
+                    self.outbound_worker_socket,
+                    self.outbound_worker_address,
+                    initiating_node=self
+                )
+                self.outbound_conn_worker_thread = threading.Thread(
+                    target=self.outbound_worker_conn_handler.run
+                )
+                self.outbound_conn_worker_thread.start()
+
+            conditional_print(f"[LOGIC] Connections between all nodes established.", VERBOSE)
+            # ONLY after all connections have been created, do the ezkl setup!
+            self.load_model()
+            # Report to coordinator that ezkl setup has been completed
+            self.coordinator_conn_handler.send("report_setup_complete")
+
+        except KeyboardInterrupt:
+            self.coordinator_conn_handler.RUNNING = False
+            self.outbound_worker_conn_handler.RUNNING = False
+            self.inbound_worker_conn_handler.RUNNING = False
+
+            self.coordinator_socket.close()
+            self.outbound_worker_socket.close()
+            self.inbound_worker_socket.close()
+
+            self.conn_coordinator_thread.join()
+            self.outbound_conn_worker_thread.join()
+            self.inbound_conn_worker_thread.join()
+            sys.exit(0)
+
+    def connect_to_coordinator(self):
+        try:
+            self.coordinator_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            self.coordinator_socket.connect(self.coordinator_address)
+            conditional_print(f"[LOGIC] Connected to Coordinator @ {self.coordinator_address}", VERBOSE)
+        except Exception as e:
+            print(e)
+            sys.exit(0)
+
+    def connect_to_worker(self):
+        try:
+            # Waits until this node has received the address it needs to connect to.
+            while self.outbound_worker_address is None:
+                if VERBOSE:
+                    print("Next worker address not set yet. Sleeping for 1s, then trying again.")
+                    time.sleep(1)
+            self.outbound_worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            self.outbound_worker_socket.connect(self.outbound_worker_address)
+            conditional_print(f"[LOGIC] Connected to Worker @ {self.outbound_worker_address}", VERBOSE)
+
+        except Exception as e:
+            print(e)
+            sys.exit(0)
+
+    def open_worker_socket(self) -> None:
+        self.inbound_worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        self.inbound_worker_socket.bind(self.address)
+        self.inbound_worker_socket.listen()
+
+    def set_ids(self, shard_id: int, model_id: str):
+        self.shard_id = shard_id
+        self.model_id = model_id
+        # The FileManager has to be initialized here, as this is the earliest time that the worker has access to both
+        # IDs.
+        self.file_manager = FileManager(
+            model_id=model_id,
+            shard_id=shard_id
+        )
+        # The Prover also has to be initialized here, as it relies on the file_manager
+        self.prover = Prover(
+            file_manager=self.file_manager,
+            input_visibility=INPUT_VISIBILITY,
+            output_visibility=OUTPUT_VISIBILITY,
+            param_visibility=PARAM_VISIBILITY,
+            ezkl_optimization_goal=OPTIMIZATION_GOAL,
+            input_scale=INPUT_SCALE,  # Optional param, can be removed.
+            param_scale=PARAM_SCALE   # Optional param, can be removed
+        )
+
+    # Loads model (shard) so that inference can be made on model (shard)
+    def load_model(self) -> None:
+        # Initialize ONNX Runtime to run inference on model (shard)
+        self.model_runtime_session = ort.InferenceSession(self.file_manager.get_model_path())
+
+        conditional_print("[PREPROCESSING] Model (shard) successfully loaded", VERBOSE)
+
+        conditional_print("[PREPROCESSING] Starting ezkl setup", VERBOSE)
+        # Set up ezkl proof artefacts needed for future inference step(s).
+        asyncio.run(self.prover.setup())
+        # conditional_print("Skipping ezkl setup for now.", VERBOSE)
+
+        conditional_print("[PREPROCESSING] ezkl setup completed", VERBOSE)
+
+    # Runs onnx model (shard) with input data, creates and writes witness file.
+    #  Takes in (base64) raw_input_data and parses into numpy array prior to further steps.
+    def run_inference(self, raw_input_data: bytes) -> None:
+        input_array: np.ndarray = decode_b64_to_np_array(raw_input_data)
+        # Run input_array (x) through model(shard) to obtain output (y).
+        output = self.model_runtime_session.run(
+            None,
+            {'input': input_array}
+        )
+
+        # The inference_run_counter, indicates in which inference "run" we currently are. Can later be used to
+        # obtain proofs of all nodes in the chain for e.g. run 55.
+        witness_id: str = f"{uuid.uuid4().hex}_{self.inference_run_counter}"
+
+        # Registers witness_id with file manager. Required before retrieving the witness path for the witness_id.
+        # Crucial step to keep track of witness_ids for post-hoc proofs of specific witness (that corresponds to
+        # specific witness_id)
+        self.file_manager.add_witness_id(witness_id)
+
+        # Generate the "raw" input-output data that is fed in the ezkl generate_witness() function.
+        raw_witness_data: dict = dict(
+            input_shapes=[input_array.shape],
+            input_data=[input_array.reshape([-1]).tolist()],
+            output_data=[o.reshape([-1]).tolist() for o in output]
+        )
+        with open(self.file_manager.get_raw_witness_path(witness_id), 'w') as f:
+            json.dump(raw_witness_data, f)
+            f.close()
+
+        # Generate witness file using ezkl.gen_witness(). Pulls in raw witness file data based on witness_id.
+        asyncio.run(self.prover.generate_witness(witness_id))
+
+        # The output data gets formatted in a list, and the actual ndarray is at index 0.
+        output_data = output[0]
+        # Encode the output data (ndarray) as base64 bytes
+        encoded_output_data: bytes = encode_np_array_to_b64(output_data)
+        # The last node send the final result to the coordinator.
+        if self.node_role == "LAST":
+            message: bytes = b'report_final_inference_output|' + self.inference_run_counter.to_bytes(8, byteorder='big') + b'|' + encoded_output_data
+            self.coordinator_conn_handler.send_bytes(message)
+        # All other nodes send their result to their neighbouring node (outbound_worker_connection).
+        else:
+            message: bytes = b'run_inference|' + encoded_output_data
+            self.outbound_worker_conn_handler.send_bytes(message)
+
+        self.coordinator_conn_handler.send(f"report_witness|{witness_id}")
+
+    # Intermediate function that calls the prover to generate a proof.
+    def generate_proof(self, witness_id: str) -> None:
+        self.prover.generate_proof_for_witness(witness_id)
+        proof_path: str = self.file_manager.get_proof_path(witness_id)
+        self.coordinator_conn_handler.send(f'report_proof|{proof_path}')
+
+
+if __name__ == "__main__":
+    # Workers need to be started in order: FIRST ... ... ... LAST
+    if len(sys.argv) != 6:
+        print(f'Usage: worker.py <worker_host> <worker_port> <coordinator_host> <coordinator_port> [node_role]')
+        print('[node_role] has 2 options: FIRST or LAST. \n'
+              'One node has to identify as the first/last, the others don\'t')
+        sys.exit(0)
+
+    worker_address = (sys.argv[1], int(sys.argv[2]))
+    coordinator_address = (sys.argv[3], int(sys.argv[4]))
+    node_role = ""
+    if len(sys.argv) > 5:
+        node_role = sys.argv[5]
+    worker = Worker(worker_address, coordinator_address, node_role)
+    worker.run()
