c02b6d9227dce5286a681d078bfadb4cf0c69079
chenneking <39014772+chenneking@users.noreply.github.com>
2024-07-11 01:14:04 -0400

    Added tests.py

---
+++ tests.py
@@ -0,0 +1,22 @@
+import unittest
+from modules.connection_handler import CoordinatorConnectionHandler
+
+class ConnectionHandlerTests(unittest.TestCase):
+    def test_run(self):
+        pass
+
+
+class CoordinatorTests(unittest.TestCase):
+    def test_b(self):
+        self.assertTrue(True)
+
+
+class WorkerTests(unittest.TestCase):
+    def test_c(self):
+        self.assertTrue(True)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
