362918c281855899e67b8d74a7fdb9d1a7dd52d0
chenneking <39014772+chenneking@users.noreply.github.com>
2024-07-12 00:03:50 -0400

    Updated visualization code to pull in and parse results csv

--- benchmarking/visualizations.py
+++ benchmarking/visualizations.py
@@ -2,30 +2,35 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-def visualize_accuracy_plot():
+
+def visualize_accuracy_plot(data_path: str):
+    df = pd.read_csv(data_path)
+
     nodes = [1, 2, 3, 4]
     reference_accuracy = [100, 100, 100, 100]
     goal_accuracy_actual_accuracy = [90, 80, 60, 50]
     goal_resources_actual_accuracy = [85, 75, 55, 45]
 
-    plt.figure(figsize=(7, 5))
-    plt.scatter(nodes, reference_accuracy, marker='o', color='orange', label='Reference')
-    plt.scatter(nodes, goal_accuracy_actual_accuracy, color='black', label='Actual (optimized for resources)')
-    plt.scatter(nodes, goal_resources_actual_accuracy, color='blue', label='Actual (optimized for accuracy)')
+    df_resources = df[df['ezkl_optimization_goal'] == 'resources']
+    df_accuracy = df[df['ezkl_optimization_goal'] == 'accuracy']
+
+    plt.figure(figsize=(9, 5))
+    #plt.scatter(df_resources['num_nodes'], df_resources['reference_accuracy_loss'], marker='o', color='orange', label='Reference')
+    plt.scatter(df_resources['num_nodes'], df_resources['accuracy_loss'], color='black', label='Actual (optimized for resources)')
+    #plt.scatter(df['num nodes'], goal_resources_actual_accuracy, color='blue', label='Actual (optimized for accuracy)')
 
     plt.xlabel('No. of nodes/shards')
-    plt.ylabel('Accuracy in %')
+    plt.ylabel('Cumulative RMSE Loss')
     plt.title('Accuracy Loss')
     plt.legend()
 
-
-    plt.yticks(list(range(0, 101, 10)))
+    #plt.yticks(df['accuracy_loss'])
     plt.xticks(nodes)
     plt.grid(False)
 
-    plt.savefig('./plots/accuracy_plot.pdf', format='pdf')
+    #plt.savefig('./plots/accuracy_plot.pdf', format='pdf')
     plt.show()
 
 
 if __name__ == '__main__':
-    visualize_accuracy_plot()
+    visualize_accuracy_plot('./results/accuracy_benchmark.csv')
