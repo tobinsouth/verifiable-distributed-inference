83b26394e261f402cabf74b3bfe0c0c47786d0f7
chenneking <39014772+chenneking@users.noreply.github.com>
2024-07-18 23:53:08 -0400

    Added global config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+"""
+Reference file structure:
+STORAGE_DIR/
+    COORDINATOR_SUB_DIR/
+        final inference outputs...
+    BENCHMARKING_RESULTS_SUB_DIR/
+        benchmarking results...
+    MODEL_SUB_DIR/
+        model_shards...
+
+    shard_0/
+    ...
+    shard_n/
+"""
+# Defines file structure
+STORAGE_DIR: str = "./shared-storage"
+MODEL_SUB_DIR: str = "/shards"
+COORDINATOR_SUB_DIR: str = "/coordinator"
+BENCHMARKING_RESULTS_SUB_DIR: str = "/benchmark_results"
+
+# Toggle for debug level prints
+VERBOSE: bool = True
+
+# ezkl configuration
+# We want the input and outputs to be "publicly" visible in the
+INPUT_VISIBILITY: str = "public"
+OUTPUT_VISIBILITY: str = "public"
+PARAM_VISIBILITY: str = "hashed"
+OPTIMIZATION_GOAL: str = "resources"
+INPUT_SCALE = None
+PARAM_SCALE = None
