0f4036a4cef3f5c1168e30b4552dbf7e5e0a63f0
chenneking <39014772+chenneking@users.noreply.github.com>
2024-07-18 23:52:00 -0400

    Updated comment

--- benchmarking/accuracy_benchmark.py
+++ benchmarking/accuracy_benchmark.py
@@ -298,7 +298,7 @@ def run_benchmark(ezkl_optimization_goal: str, num_nodes: int, model_name: str) 
 
 if __name__ == '__main__':
     # Example usage:
-    # python accuracy_benchmark.py linear_relu ./tmp
+    # python accuracy_benchmark.py mlp ./tmp
     # python accuracy_benchmark.py cnn ./tmp2
     # python accuracy_benchmark.py attention ./tmp3
 
